7e38a18b5ba61123f0cbde8ebf8b3f8197a5f46f
noprd <noprd@users.noreply.github.com>
2024-08-23 13:51:37 +0200

    story-structs > staging: minor

--- src/main.zig
+++ src/main.zig
@@ -13,7 +13,11 @@ pub fn main() !void {
     const allocator = std.heap.page_allocator;
     var args = try std.process.ArgIterator.initWithAllocator(allocator);
     defer args.deinit();
+
+    // ignore first argument
     _ = args.next();
+
+    // get second argument
     if (args.next()) |name| {
         const user = users.User{ .name = name };
         user.greet();
